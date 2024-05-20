ace0ef53d59def8e06fc9d6eb91f773c535e6377
Robson Rocha <135916984+robsonrccha@users.noreply.github.com>
2024-05-20 12:26:34 -0300

    "Update"

---
+++ ContaBanco/src/robsonrccha/com/github/ContaTerminal.java
@@ -0,0 +1,17 @@
+package robsonrccha.com.github;
+
+import java.util.Scanner;
+
+public class ContaTerminal {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		
+		
+		
+		
+		sc.close();
+
+	}
+
+}
